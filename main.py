3fd1cafc90eca818409755981438aabe3cf2cc12
bazunaka <bazunaka32@gmail.com>
2023-04-08 23:54:01 +0300

    add new get_data

--- main.py
+++ main.py
@@ -9,23 +9,45 @@ def get_html(url):
 
 def get_data(html):
     soup = BeautifulSoup(html, "lxml")
-    h3 = soup.find(
+    ul = soup.find(
+        'ul',
+        {"class": "ez-toc-list ez-toc-list-level-1"}
+    )
+    li = ul.find_all('li')
+
+    for a in li:
+        name = a.find_next('a').text
+        print(name)
+
+
+def get_workouts(html):
+    soup = BeautifulSoup(html, "lxml")
+    div = soup.find(
         'div',
-        {"class": "td-post-content tagdiv-type"}
-    ).find('h3').text
-    return h3
+        {"class": "ui two stackable cards"}
+    )
+    a = div.findAll(
+        'div',
+        {"class": "content"}
+    )
+    for aq in a:
+        name = aq.find_next('a').text
+        print(name)
 
 
 def get_multiple_data(html):
     soup = BeautifulSoup(html, "lxml")
-    h3 = soup.findAll('h3')[3]
-    h3_text = h3.findAllNext('a')
-    return h3_text
+    h3 = soup.findAll('li')
+    # h3_text = h3.findAllNext('li')
+    return dir(h3)
 
 
 def main():
-    url = 'https://pumpmuscles.ru/bodibilding/anatomiya-myishts-cheloveka-bodibildera.html#Трапеции'
-    print(get_multiple_data(get_html(url)))
+    # url = 'https://pumpmuscles.ru/bodibilding/anatomiya-myishts-cheloveka-bodibildera.html#Трапеции'
+    # print(get_multiple_data(get_html(url)))
+    # url = 'https://dailyfit.ru/katalog-uprazhnenij/?tax_celevye_myshcy%5B%5D=44&tax_vid_uprazhnenija%5B%5D=18' грудь
+    url = 'https://dailyfit.ru/katalog-uprazhnenij/?tax_celevye_myshcy%5B%5D=31&tax_vid_uprazhnenija%5B%5D=18'
+    print(get_workouts(get_html(url)))
 
 
 if __name__ == '__main__':
