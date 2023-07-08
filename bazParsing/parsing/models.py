9854250d073c695e8ad1ce5f75b454c0d5ddc8ed
bazunaka <bazunaka32@gmail.com>
2023-07-08 14:15:51 +0300

    first migration

--- bazParsing/parsing/models.py
+++ bazParsing/parsing/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Apartment(models.Model):
+    name = models.CharField(max_length=50)
+    square = models.FloatField()
+    price = models.IntegerField()
+    date = models.DateField()
+    decor = models.BooleanField()
+    date_view = models.DateField()
+    is_metro = models.BooleanField()
+    metro_name = models.CharField(max_length=50, blank=True)
+    is_mcd = models.BooleanField()
+    mcd_name = models.CharField(max_length=50, blank=True)
+    rent = models.IntegerField()
+    mortgage = models.IntegerField()
