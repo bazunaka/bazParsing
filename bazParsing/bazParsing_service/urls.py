3a20e21eb1509c6db716d88fe4b10135286335f5
bazunaka <bazunaka32@gmail.com>
2023-07-15 15:52:46 +0300

    view model

--- bazParsing/bazParsing_service/urls.py
+++ bazParsing/bazParsing_service/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from .views import home_views
+from parsing.views import home_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home_views),
+    path('home/', home_view),
 ]
--- bazParsing/parsing/views.py
+++ bazParsing/parsing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Development, Apartment
+
 
 # Create your views here.
+
+def home_view(request):
+    query_set = Development.objects.all()
+    return render(request, 'home.html', {'object_list': query_set})
