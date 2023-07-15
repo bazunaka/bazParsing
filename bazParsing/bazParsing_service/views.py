fa3454262132640f2a53e32e8c6b5b6238af4500
bazunaka <bazunaka32@gmail.com>
2023-07-15 22:08:26 +0300

    add base html

--- bazParsing/bazParsing_service/views.py
+++ bazParsing/bazParsing_service/views.py
@@ -6,4 +6,4 @@ def home_views(request):
     date = datetime.datetime.now().date()
     name = 'Test'
     _context = {'date': date, 'name': name}
-    return render(request, 'home.html', _context)
+    return render(request, 'base.html', _context)
--- bazParsing/parsing/views.py
+++ bazParsing/parsing/views.py
@@ -5,5 +5,5 @@ from .models import Development, Apartment
 # Create your views here.
 
 def home_view(request):
-    query_set = Development.objects.all()
+    query_set = Apartment.objects.all()
     return render(request, 'home.html', {'object_list': query_set})
