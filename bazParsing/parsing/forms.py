bfcdc605457b822315cbed57d66135bd8fdec311
bazunaka <bazunaka32@gmail.com>
2023-07-17 15:11:18 +0300

    add widget

--- bazParsing/parsing/forms.py
+++ bazParsing/parsing/forms.py
@@ -3,4 +3,7 @@ from .models import Development
 
 
 class FindForm(forms.Form):
-    development = forms.ModelChoiceField(queryset=Development.objects.all(), to_field_name='name')
+    development = forms.ModelChoiceField(queryset=Development.objects.all(), to_field_name='name',
+                                         widget=forms.Select(attrs={'class': 'form-control'}),
+                                         label='Застройщик')
+
